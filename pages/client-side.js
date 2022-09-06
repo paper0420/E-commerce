46925a288724c05c39a95d31bf340fa701727943
paper0420 <sumitra0420@gmail.com>
2022-09-06 14:18:55 +0700

    comment clientside

--- pages/client-side.js
+++ pages/client-side.js
@@ -1,47 +1,47 @@
-import axios from "axios";
-import { useState,useEffect } from "react";
+// import axios from "axios";
+// import { useState,useEffect } from "react";
 
-const url = "https://pokeapi.co/api/v2/pokemon?limit=150&offset=0";
-const headers = {
-    'Cache-Control': "no-cach",
-}
-const ClientSide = () => {
-    const [pokemon,setPokemon] = useState([]);
+// const url = "https://pokeapi.co/api/v2/pokemon?limit=150&offset=0";
+// const headers = {
+//     'Cache-Control': "no-cach",
+// }
+// const ClientSide = () => {
+//     const [pokemon,setPokemon] = useState([]);
 
-    useEffect(()=>{
-        const fetchPokemon = async () => {
+//     useEffect(()=>{
+//         const fetchPokemon = async () => {
 
-            const response = await axios.get(url,{headers});
-            const promises = response.data.results.map((result) => {
-                return axios.get(result.url,{headers});
-            });
+//             const response = await axios.get(url,{headers});
+//             const promises = response.data.results.map((result) => {
+//                 return axios.get(result.url,{headers});
+//             });
 
-            const responses = await Promise.all(promises);
-            const pokeData = responses.map((r)=>{
-                return {
-                    name: r.data.name,
-                    imgUrl: r.data.sprites.front_default
-                };
+//             const responses = await Promise.all(promises);
+//             const pokeData = responses.map((r)=>{
+//                 return {
+//                     name: r.data.name,
+//                     imgUrl: r.data.sprites.front_default
+//                 };
 
-            });
-            setPokemon(pokeData);
+//             });
+//             setPokemon(pokeData);
 
-        }
+//         }
 
-        fetchPokemon();
+//         fetchPokemon();
 
-    },[])
-    console.log(pokemon);
-    return (
-        pokemon.map((poke)=>{
-            return(
-                <div key={poke.name}>
-                    <img src={poke.imgUrl}/>
-                    <p>{poke.name}</p>
-                </div>
-            )
-        })
-    )
-}
+//     },[])
+//     console.log(pokemon);
+//     return (
+//         pokemon.map((poke)=>{
+//             return(
+//                 <div key={poke.name}>
+//                     <img src={poke.imgUrl}/>
+//                     <p>{poke.name}</p>
+//                 </div>
+//             )
+//         })
+//     )
+// }
 
-export default ClientSide;
+// export default ClientSide;
