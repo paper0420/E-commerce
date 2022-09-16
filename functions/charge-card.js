7ff7298e9748f2a3acf83c1b3481b6a2e520f652
paper0420 <sumitra0420@gmail.com>
2022-09-16 17:44:03 +0700

    add netlify function

---
+++ functions/charge-card.js
@@ -0,0 +1,10 @@
+exports.handler = async (event,context) => {
+    //talking to Stripe
+    //Charging the card 
+    return {
+        statusCode: 200,
+        body: 'I have charged that card!'
+
+    }
+
+}
---
+++ pages/cancelled.js
@@ -0,0 +1,11 @@
+const Cancelled = () => {
+    return (
+        <div>
+            <h1>Payment Cancelled!</h1>
+            <p>You have not been charge!</p>
+        </div>
+
+    )
+
+}
+export default Cancelled;
