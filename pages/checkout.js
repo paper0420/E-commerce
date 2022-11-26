8a7003c8f86cadf270eaa11444c7b91fb0f16d61
Nik P <niaher.crimston@gmail.com>
2022-11-26 19:22:57 +0700

    fix: make stripe redirect work

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,46 +1,48 @@
 import useCart from '../hooks/useCart';
-import styles from './checkout.module.scss'
-import {loadStripe} from '@stripe/stripe-js'
+import styles from './checkout.module.scss';
 
 const Checkout = () => {
     const { cart, total } = useCart();
 
     const processPayment = async () => {
-        const url = "/.vercel/api/checkout_session";
-        const newCart = cart.map(({id,qty})=>({
+        const cartItems = cart.map(({ id, qty }) => ({
             id,
             qty,
         }));
-        const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
-        const {data} = await fetch(url,{cart:newCart});
 
-        //await stripe.redirectToCheckout({sessionId:data.id});
+        const response = await fetch('/api/checkout_session', {
+            method: 'POST',
+            body: JSON.stringify(cartItems)
+        });
+
+        var url = await response.json();
+        window.location = url;
     };
 
     return (
         <div>
             <h1>Checkout</h1>
             {cart.length > 0 ? (
-            <div>
-                <ul className={styles.ul}>
+                <div>
+                    <ul className={styles.ul}>
                         {cart.map((item) => {
-                    return (
-                    <li className={styles.item} key={item.id}>
-                        <span>{item.qty} X {item.name}</span>
+                            return (
+                                <li className={styles.item} key={item.id}>
+                                    <span>{item.qty} X {item.name}</span>
                                     <span>${item.price / 100}</span>
-                    </li>
-                    )
-                })}
-                </ul>
-                <div className={styles.total}>
-                    <span>Total</span>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <div className={styles.total}>
+                        <span>Total</span>
                         <span>${total / 100}</span>
+                    </div>
+                    <button className={styles.checkoutButton} onClick={processPayment}>Process Payment</button>
                 </div>
-                <button className={styles.checkoutButton} onClick={processPayment}>Process Payment</button>
-            </div>
             ) : (
                 <p>No item</p>
-            )}           
+            )}
 
         </div>
 
