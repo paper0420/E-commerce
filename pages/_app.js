09079f86a6e702506d90ff45c8ede2dabf8c8e96
Paper0420 <sumitra0420@gmail.com>
2022-12-10 23:26:06 +0700

    fix: move Script outside Head

--- pages/_app.js
+++ pages/_app.js
@@ -32,17 +32,18 @@ function MyApp({ Component, pageProps, session }) {
                 name="viewport"
                 content="width=device-width, initial-scale=1"
               />
-              <Script
-                src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-                integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-                crossorigin="anonymous"
-              ></Script>
-              <Script
-                src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-                integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-                crossorigin="anonymous"
-              ></Script>
             </Head>
+
+            <Script
+              src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+              integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+              crossorigin="anonymous"
+            ></Script>
+            <Script
+              src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+              integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+              crossorigin="anonymous"
+            ></Script>
 
             <div className="container-sm mainBgAndFont">
               <Navbar cart={isOpen} openCart={openCart} />
