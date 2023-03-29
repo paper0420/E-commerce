34a4e6a45033850c9b3214513cc1988eb532006a
Paper0420 <sumitra0420@gmail.com>
2023-03-29 15:51:53 +1100

    Remove try catch

--- pages/api/getAddress.js
+++ pages/api/getAddress.js
@@ -1,42 +1,38 @@
 import conn from "../../lib/db";
 
 export default async (req, res) => {
-  try {
-    const input = JSON.parse(req.body);
+  const input = JSON.parse(req.body);
 
-    const accountData = await conn.query('SELECT * FROM "Account" ');
-    const accountId = accountData.rows.filter(
-      (item) => item.Email === input.email
-    )[0].Id;
+  const accountData = await conn.query('SELECT * FROM "Account" ');
+  const accountId = accountData.rows.filter(
+    (item) => item.Email === input.email
+  )[0].Id;
 
-    const addressData = await conn.query('SELECT * FROM "Address" ');
+  const addressData = await conn.query('SELECT * FROM "Address" ');
 
-    const isAccountIDExist = addressData.rows.some(
-      (item) => item.AccountId === accountId
+  const isAccountIDExist = addressData.rows.some(
+    (item) => item.AccountId === accountId
+  );
+
+  let data = {
+    address: null,
+    accountId: accountId,
+  };
+
+  if (!isAccountIDExist) {
+    console.log("isAccountIDExist" + isAccountIDExist);
+    res.status(200).json(data);
+  } else {
+    console.log("isAccountIDExist" + isAccountIDExist);
+
+    const address = await conn.query(
+      'SELECT "HouseId", "Street", "City", "ZipCode", "Country", "Phonenumber" FROM "Address" WHERE "AccountId" = $1 ',
+      [accountId]
     );
-
-    let data = {
-      address: null,
+    data = {
+      address: address.rows[0],
       accountId: accountId,
     };
-
-    if (!isAccountIDExist) {
-      console.log("isAccountIDExist" + isAccountIDExist);
-      res.status(200).json(data);
-    } else {
-      console.log("isAccountIDExist" + isAccountIDExist);
-
-      const address = await conn.query(
-        'SELECT "HouseId", "Street", "City", "ZipCode", "Country", "Phonenumber" FROM "Address" WHERE "AccountId" = $1 ',
-        [accountId]
-      );
-      data = {
-        address: address.rows[0],
-        accountId: accountId,
-      };
-      res.status(200).json(data);
-    }
-  } catch (error) {
-    console.log(error);
+    res.status(200).json(data);
   }
 };
